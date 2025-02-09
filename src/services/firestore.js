5ac6af1abc5d6e2776f3b9a82086b8000f2ac8b7
CPeculiar <pecudamicable@gmail.com>
2025-02-09 14:25:49 +0100

    git ignore

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -22,13 +22,13 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBaJLXoCDoz9W5wuSPrPVslsLOiAVraOJQ",
-  authDomain: "tech-academy-8a341.firebaseapp.com",
-  projectId: "tech-academy-8a341",
-  storageBucket: "tech-academy-8a341.firebasestorage.app",
-  messagingSenderId: "258764322577",
-  appId: "1:258764322577:web:4d941122a1c36cd12b3eb0",
-  measurementId: "G-C52038MYF7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
